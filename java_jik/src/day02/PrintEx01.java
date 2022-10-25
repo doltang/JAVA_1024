93e33aba7e3c01fca8f8bd0f261670d2fc6e540f
Doltang <doltangdoltang@gmail.com>
2022-10-25 18:09:21 +0900

    자바 02일차: 콘솔 출력 예제

---
+++ java_jik/src/day02/PrintEx01.java
@@ -0,0 +1,26 @@
+package day02;
+
+public class PrintEx01 {
+
+	public static void main(String[] args) {
+		/* println() : 원하는 값을 출력한 후 엔터
+		 * 				()안에 값을 생략 가능
+		 * print() : 원하는 값을 출력 
+		 * 				() 안에 값을 생략할 수 없음
+		 * 문자열 + 정수 => 문자열
+		 * 정수 + 문자열 => 문자열
+		 * 문자 + 문자열 => 문자열
+		 * */
+		String name = "홍길동";
+		int age = 20;
+		System.out.println(name + "의 나이는 " + age + "살 입니다.");// 문자열
+		
+		int num1 = 10;
+		int num2 = 20;
+		char operator = '+'; // 유니코드 43 
+		// 10 + 20
+		System.out.println("" + num1 + operator + num2); // 정수 + 문자 => 정수
+
+	}
+
+}
