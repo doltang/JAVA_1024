14591371ca11cadf5af69ebe55338329896201f3
Doltang <doltangdoltang@gmail.com>
2023-02-02 21:35:48 +0900

    프론트 22일차

--- front/css/naver2/naver.js
+++ front/css/naver2/naver.js
@@ -1,3 +1,4 @@
+/* 롤링 관련 이벤트 */
 $(function(){
 	$('.box-body-right2 .btn-next').click(function(e){
 		e.preventDefault();
@@ -8,7 +9,6 @@ $(function(){
 		e.preventDefault();
 		moveRight(liRight2, ulRight2, timeRight2);
 	});
-	
 	
 	$('.box-body-right2').hover(function(){
 		clearInterval(rollingRight2);
@@ -23,6 +23,96 @@ $(function(){
 	});
 	
 });
+//페이지네이션 버튼 이벤트
+$(function(){
+	$('.box-shop-in .btn-prev').click(function(e){
+		e.preventDefault();
+		let boxShopIn = $(this).parents('.box-shop-in');
+		let curPage = boxShopIn.find('.current-page').first().text();
+		let maxPage = boxShopIn.find('.max-page').first().text();
+		curPage = +curPage;
+		curPage = --curPage == 0 ? maxPage : curPage;
+		boxShopIn.find('.current-page').text(curPage);
+	});
+	$('.box-shop-in .btn-next').click(function(e){
+		e.preventDefault();
+		let boxShopIn = $(this).parents('.box-shop-in');
+		let curPage = boxShopIn.find('.current-page').first().text();
+		let maxPage = boxShopIn.find('.max-page').first().text();
+		curPage = +curPage;
+		curPage = ++curPage > maxPage ? 1 : curPage;
+		boxShopIn.find('.current-page').text(curPage);
+	});
+});
+//왼쪽 두번째 뉴스 리스트 버튼 관리
+$(function(){
+
+	selectNewsList($('.box-body-left2 .list-press').eq(3))
+
+	$('.box-body-left2 .btn-prev').click(function(e){
+		e.preventDefault();
+		let obj = $('.box-body-left2 .list-press.selected');
+		if(obj.prev().length != 0){
+			selectNewsList(obj.prev());
+		}
+	});
+	$('.box-body-left2 .btn-next').click(function(e){
+		e.preventDefault();
+		let obj = $('.box-body-left2 .list-press.selected');
+		if(obj.next().length == 0 || !obj.next().hasClass('list-press'))
+			return;
+		selectNewsList(obj.next());
+	});
+});
+//왼쪽3번째 컨텐츠 관련 이벤트
+$(function(){
+	$('.box-body-left3 .box-menu .btn-menu').click(function(e){
+		e.preventDefault();
+		$('.box-body-left3 .box-menu .item-menu .btn-menu').attr('aria-selected',false);
+		$(this).attr('aria-selected',true);
+		if($(this).parent().prev().length == 0){
+			$('.box-body-left3 .btn-prev').hide();
+		}else{
+			$('.box-body-left3 .btn-prev').show();
+		}
+		if($(this).parent().next().length == 0){
+			$('.box-body-left3 .btn-next').hide();
+		}else{
+			$('.box-body-left3 .btn-next').show();
+		}
+	})
+	$('.box-body-left3 .btn-next').click(function(e){
+		e.preventDefault();
+		let obj = $('.box-body-left3 .box-menu .item-menu .btn-menu').filter('[aria-selected=true]')
+		if(obj.parent().next().length != 0)
+			obj.parent().next().children().click();
+		
+		if(obj.hasClass('living')){
+			obj.parents('.list-menu').animate({
+				marginLeft : '-185px'
+			}, 500);
+		}
+	})
+	$('.box-body-left3 .btn-prev').click(function(e){
+		e.preventDefault();
+		let obj = $('.box-body-left3 .box-menu .item-menu .btn-menu').filter('[aria-selected=true]')
+		if(obj.parent().prev().length != 0)
+			obj.parent().prev().children().click();
+		
+		if(obj.hasClass('car')){
+			obj.parents('.list-menu').animate({
+				marginLeft : '0px'
+			}, 500);
+		}
+	})
+});
+//메뉴 관련 이벤트
+$(function(){
+	$('.group-menu .btn-more').click(function(e){
+		e.preventDefault();
+		$(this).toggleClass('fold');
+	})
+})
 
 let liRight2 = '.box-body-right2 .item-stock';
 let ulRight2 = '.box-body-right2 .list-stock';
@@ -84,19 +174,18 @@ function rollingTop(rollingObj){
 	return setInterval(moveTop,rollingObj.duration,rollingObj.liSelector, 
 		rollingObj.ulSelector, rollingObj.animationTime);
 }
-
-$('.btn-prev').click(()=>{
-	let curPage = $('.page').text();//.data('page');
-	let maxPage = $('.max-page').text();//.data('max');
-	curPage = +curPage;
-	curPage = --curPage == 0 ? maxPage : curPage;
-	$('.page').text(curPage);
-});
-$('.btn-next').click(()=>{
-	let curPage = $('.page').text();//.data('page');
-	let maxPage = $('.max-page').text();//.data('max');
-	curPage = +curPage;
-	curPage = ++curPage > maxPage ? 1 : curPage;
-	$('.page').text(curPage);
-});
-
+function selectNewsList(el){
+	$('.box-body-left2 .list-press').removeClass('selected').hide();
+	el.show();
+	el.addClass('selected')
+	if(el.prev().length == 0){
+		$('.box-body-left2 .btn-prev').hide();
+	}else{
+		$('.box-body-left2 .btn-prev').show();
+	}
+	if(el.next().length == 0 || !el.next().hasClass('list-press')){
+		$('.box-body-left2 .btn-next').hide();
+	}else{
+		$('.box-body-left2 .btn-next').show();
+	}
+}
