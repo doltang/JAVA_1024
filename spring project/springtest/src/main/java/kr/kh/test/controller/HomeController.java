1ae3ef96a2aa2fcef3feef727ba16708da1d0cd1
Doltang <doltangdoltang@gmail.com>
2023-02-20 19:52:14 +0900

    스프링 8일차 : 회원가입 복습

--- spring project/springtest/src/main/java/kr/kh/test/controller/HomeController.java
+++ spring project/springtest/src/main/java/kr/kh/test/controller/HomeController.java
@@ -1,12 +1,17 @@
 package kr.kh.test.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import kr.kh.test.service.MemberService;
+import kr.kh.test.vo.MemberVO;
 
 
 @Controller
@@ -18,9 +23,29 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ModelAndView home(ModelAndView mv, Integer num) {
 		String name = memberService.selectMemberName(num);
-		System.out.println(name);
 		mv.setViewName("/main/home");
 		return mv;
 	}
+	
+	 @RequestMapping(value= "/signup", method = RequestMethod.GET)
+	 public ModelAndView signup(ModelAndView mv) {
+		 mv.setViewName("/member/signup");
+		 return mv;
+	 }
+	 
+	 @RequestMapping(value= "/signup", method = RequestMethod.POST)
+	 public ModelAndView signuPost(ModelAndView mv, MemberVO member) {
+		boolean isSignup = memberService.signup(member);
+		if(isSignup) {				
+			//성공했다고 알림 메세지(추후 구현 예정)
+			System.out.println("회원가입이 완료되었습니다.");
+			mv.setViewName("redirect:/");
+		}else {
+			//성공했다고 알림 메세지(추후 구현 예정)
+			System.out.println("회원가입에 실패했습니다.");
+			mv.setViewName("redirect:/signup");
+		}
+		return mv;
+	 }
 	
 }
---
+++ spring project/springtest/src/main/java/kr/kh/test/vo/MemberVO.java
@@ -0,0 +1,27 @@
+package kr.kh.test.vo;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import lombok.Data;
+
+@Data	
+public class MemberVO {
+	String me_id;
+	String me_pw;
+	String me_email;
+	Date me_birthday;
+	int me_authority;
+	Date me_join_time;
+	
+	public void setMe_birthday(String me_birthday) {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		try {
+			this.me_birthday = format.parse(me_birthday);
+		} catch (ParseException e) {
+			// 날짜 형태가 잘못되었을 경우 null로 저장
+			this.me_birthday = null;
+		}
+	}
+}
